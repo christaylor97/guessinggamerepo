f872dbba45a404858792b596b19fffa745335b5f
christaylor97 <christytaylor396@btinternet.com>
2019-08-09 17:53:43 +0100

    wrote most of guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,8 +1,26 @@
-echo "Guess the number of files in the directory"
-while [[$1 -gt 3 ]]
-do
-	echo "That number is too high!"
-while [[$1 -lt 3 ]]
-do
-	echo "That number is too low!"
-done
+echo "Guess the number of files in the current directory"
+read response
+
+function guessinggame {
+	while [[ $response -ne $answer ]]
+	do
+		if [[ $response -gt $answer ]]
+		then
+			echo "That number is too high! Please try again."
+			read response
+		elif [[ $response -lt $answer ]]
+		then
+			echo "That number is too low! Please try again."
+			read response
+		fi
+	done
+	if [[ $response -eq $answer ]]
+	then
+		echo "$1 is correct! Well done!"
+		echo "Thank you for playing."
+	fi
+}
+
+$answer = $(ls | wc - 1)
+guessinggame response answer
+
