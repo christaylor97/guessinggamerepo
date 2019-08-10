4e2faf3d8767ec401632cabdda851ff0c1fcff62
christaylor97 <christytaylor396@btinternet.com>
2019-08-10 17:31:39 +0100

    finished makefile and guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,26 +1,29 @@
 echo "Guess the number of files in the current directory"
-read response
 
-function guessinggame {
-	while [[ $response -ne $answer ]]
+guessinggame()
+{
+	let answer=$(ls | wc -l)
+
+	read response
+	while [[ response -ne $answer ]]
 	do
-		if [[ $response -gt $answer ]]
+		if [[ response -gt $answer ]]
 		then
 			echo "That number is too high! Please try again."
 			read response
-		elif [[ $response -lt $answer ]]
+		elif [[ response -lt $answer ]]
 		then
 			echo "That number is too low! Please try again."
 			read response
 		fi
 	done
-	if [[ $response -eq $answer ]]
+
+	if [[ response -eq $answer ]]
 	then
-		echo "$1 is correct! Well done!"
+		echo "$response is correct! Well done!"
 		echo "Thank you for playing."
 	fi
 }
+guessinggame
 
-$answer = $(ls | wc - 1)
-guessinggame response answer
 
---
+++ makefile
@@ -0,0 +1,9 @@
+README.md:
+	touch README.md
+	echo "Unix Workbench Project: Guessing Game" >> README.md
+	echo "Date at which make was run:"
+	date >> README.md
+	echo "Number of lines of code contained in guessinggame.sh:" >> README.md
+	wc -l guessinggame.sh | egrep -o "[0-9]+" >> README.md
+	
+	
